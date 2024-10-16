2463874e004b5e3dc9b1a7bca351e7a349c653e9
mirolis6636 <mirolis6636@gmail.com>
2024-10-16 16:11:13 +0900

    Third message

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
   public static void main(String[] args) {
     System.out.println("Hello sample");
     System.out.println("hello second");
+    System.out.println("hello third");
   }
 }
