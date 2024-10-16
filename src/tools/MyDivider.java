f05f7234307c2d7d373787a8591120d835c5946c
mirolis6636 <mirolis6636@gmail.com>
2024-10-16 22:29:17 +0900

    Add MyDivider class, exception check

--- src/tools/MyDivider.java
+++ src/tools/MyDivider.java
@@ -1,7 +1,9 @@
 package tools;
 
 public class MyDivider {
-  double divider(int a, int b){
+  double divider(int a, int b) throws IllegalArgumentException{
+    if(b==0)
+      throw new IllegalArgumentException();
     return a/b;
   }
 }
