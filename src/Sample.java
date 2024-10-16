0474ae969c0f08d8e358ce39ba7e639d3a62f823
mirolis6636 <mirolis6636@gmail.com>
2024-10-16 22:37:27 +0900

    Complete my project

--- src/Sample.java
+++ src/Sample.java
@@ -1,7 +1,27 @@
+import tools.MyAdder;
+import tools.MyCalculator;
+import tools.MyDivider;
+
+import java.util.Scanner;
+
 public class Sample {
   public static void main(String[] args) {
     System.out.println("Hello sample");
     System.out.println("hello second");
     System.out.println("hello third");
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println("(1) Adder, (2) Divider");
+    int input = sc.nextInt();
+    if(input==1){
+      MyAdder mc = new MyAdder();
+      System.out.println(mc.adder(a,b));
+    }
+    else if(input == 2){
+      MyDivider md = new MyDivider();
+      System.out.println(md.divider(a,b));
+    }
   }
 }
--- src/tools/MyDivider.java
+++ src/tools/MyDivider.java
@@ -1,7 +1,7 @@
 package tools;
 
 public class MyDivider {
-  double divider(int a, int b) throws IllegalArgumentException{
+  public double divider(int a, int b) throws IllegalArgumentException{
     if(b==0)
       throw new IllegalArgumentException();
     return a/b;
