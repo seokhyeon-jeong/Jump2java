f4b939efffd90e8a278da57473b73eaea77d6e3b
mirolis6636 <mirolis6636@gmail.com>
2024-10-17 15:36:13 +0900

    refactor: delete all the files

--- src/tools/MyDivider.java
+++
@@ -1,9 +0,0 @@
-package tools;
-
-public class MyDivider {
-  public double divider(int a, int b) throws IllegalArgumentException{
-    if(b==0)
-      throw new IllegalArgumentException();
-    return a/b;
-  }
-}
